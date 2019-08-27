9da6dea04dfbc5819d58bd3f853a71dcf0261467
Scott Finlay <scott.finlay@sixt.com>
2019-08-27 12:40:09 +0200

    Trying to fix clz junk

--- clz_amd64.s
+++ clz_amd64.s
@@ -1,23 +1,13 @@
 #include "textflag.h"
 
 // func clz(x int64) (n int64)
-TEXT ·clz(SB),NOSPLIT,$0
-	BSRQ 	x+0(FP), AX
-	JZ 		Z1
-	XORQ	$63, AX
-	MOVQ 	AX, n+8(FP)
-	RET
-
-Z1:	MOVQ 	$0, n+8(FP)
-	RET
-
-// func ctz(x int64) (n int64)
-TEXT ·ctz(SB),NOSPLIT,$0
-	BSRQ 	x+0(FP), AX
-	JZ 		Z1
-	ADDQ	$1, AX
-	MOVQ 	AX, n+8(FP)
-	RET
-
-Z1:	MOVQ 	$0, n+8(FP)
-	RET
+TEXT ·Clz(SB),4,$0-16
+        BSRQ  x+0(FP), AX
+        JZ zero
+        SUBQ  $63, AX
+        NEGQ AX
+        MOVQ AX, ret+8(FP)
+        RET
+zero:
+        MOVQ $64, ret+8(FP)
+        RET
